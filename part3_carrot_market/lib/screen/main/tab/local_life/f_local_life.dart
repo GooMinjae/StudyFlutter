c1edef8719d1a28d9bafcdbd91f0e7e881345620
ming <999aldwo@gmail.com>
2025-01-13 13:57:08 +0900

    bottom navigation 설정

---
+++ part3_carrot_market/lib/screen/main/tab/local_life/f_local_life.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LocalLiftFragment extends StatefulWidget {
+  const LocalLiftFragment({super.key});
+
+  @override
+  State<LocalLiftFragment> createState() => _LocalLiftFragmentState();
+}
+
+class _LocalLiftFragmentState extends State<LocalLiftFragment> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
