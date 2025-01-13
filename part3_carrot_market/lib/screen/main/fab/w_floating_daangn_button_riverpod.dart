b6a88ea8b5d7dcbc5ff149b8d47185c5d3cf756b
ming <999aldwo@gmail.com>
2025-01-13 17:20:46 +0900

    floating action button

---
+++ part3_carrot_market/lib/screen/main/fab/w_floating_daangn_button_riverpod.dart
@@ -0,0 +1,96 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'w_floating_daangn_button.state.dart';
+
+final floatingButtonStateProvider =
+    StateNotifierProvider<FloatingButtonStateNotifier, FloatingButtonState>(
+  (ref) => FloatingButtonStateNotifier(
+    const FloatingButtonState(false, false),
+  ),
+);
+
+class FloatingButtonStateNotifier extends StateNotifier<FloatingButtonState> {
+  FloatingButtonStateNotifier(super.state);
+
+  bool needToMakeButtonBigger = false;
+
+  void onTapButton() {
+    final isExpanded = state.isExpanded;
+    final isSmall = state.isSmall;
+
+    // state = state.copyWith(
+    //     isExpanded: !state.isExpanded,
+    //     isSmall: needToMakeButtonBigger ? false : true);
+
+    // if (needToMakeButtonBigger) {
+    //   needToMakeButtonBigger = false;
+    // }
+
+    // if (!isSmall && !isExpanded) {
+    //   needToMakeButtonBigger = true;
+    // }
+    state = state.copyWith(
+        isExpanded: !state.isExpanded,
+        isSmall: needToMakeButtonBigger ? false : true);
+
+    if (needToMakeButtonBigger) {
+      needToMakeButtonBigger = false;
+    }
+    if (!isSmall && !isExpanded) {
+      needToMakeButtonBigger = true;
+    }
+  }
+
+  void changeButtonSize(bool isSmall) {
+    if (state.isExpanded) {
+      return;
+    }
+
+    state = state.copyWith(isSmall: isSmall);
+  }
+}
+// import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+// class FloatingButtonState {
+//   bool isExpanded;
+//   bool isSmall;
+
+//   FloatingButtonState()
+//       : isExpanded = false,
+//         isSmall = false;
+// }
+
+// final floatingButtonStateProvider =
+//     StateNotifierProvider<FloatingButtonStateNotifier, FloatingButtonState>(
+//   (ref) => FloatingButtonStateNotifier(
+//     FloatingButtonState(),
+//   ),
+// );
+
+// class FloatingButtonStateNotifier extends StateNotifier<FloatingButtonState> {
+//   FloatingButtonStateNotifier(super.state);
+
+//   @override
+//   bool updateShouldNotify(
+//       FloatingButtonState old, FloatingButtonState current) {
+//     // TODO: implement updateShouldNotify
+//     return true;
+//   }
+
+//   void onTapButton() {
+//     // state.isExpanded = !state.isExpanded;
+//     // state.isSmall = true;
+//     // ==
+//     state = state
+//       ..isExpanded = !state.isExpanded
+//       ..isSmall = true;
+//   }
+
+//   void changeButtonSize(bool isSmall) {
+//     state = state..isSmall = isSmall;
+//   }
+// }
+
+// // final floatingButtonIsExpandedProvider = StateProvider((ref) => false);
+// // final floatingButtonIsSmallProvider = StateProvider((ref) => false);
+
--- part3_carrot_market/lib/screen/main/tab/local_life/f_local_life.dart
+++ part3_carrot_market/lib/screen/main/tab/local_life/f_local_life.dart
@@ -1,15 +1,52 @@
+import 'package:fast_app_base/screen/main/fab/w_floating_daangn_button_riverpod.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class LocalLiftFragment extends StatefulWidget {
+class LocalLiftFragment extends ConsumerStatefulWidget {
   const LocalLiftFragment({super.key});
 
   @override
-  State<LocalLiftFragment> createState() => _LocalLiftFragmentState();
+  ConsumerState<LocalLiftFragment> createState() => _LocalLiftFragmentState();
 }
 
-class _LocalLiftFragmentState extends State<LocalLiftFragment> {
+class _LocalLiftFragmentState extends ConsumerState<LocalLiftFragment> {
+  final scrollController = ScrollController();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    scrollController.addListener(() {
+      final floatingButtonState = ref.watch(floatingButtonStateProvider);
+
+      if (scrollController.position.pixels > 100 &&
+          !floatingButtonState.isSmall) {
+        ref.read(floatingButtonStateProvider.notifier).changeButtonSize(true);
+      } else if (scrollController.position.pixels < 100 &&
+          floatingButtonState.isSmall) {
+        ref.read(floatingButtonStateProvider.notifier).changeButtonSize(false);
+      }
+    });
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return ListView(
+      controller: scrollController,
+      children: [
+        Container(
+          height: 500,
+          color: Colors.orange,
+        ),
+        Container(
+          height: 500,
+          color: Colors.green,
+        ),
+        Container(
+          height: 500,
+          color: Colors.red,
+        )
+      ],
+    );
   }
 }
