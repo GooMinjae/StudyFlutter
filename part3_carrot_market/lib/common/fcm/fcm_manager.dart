976f6d8513ca5079e4fc7cd597ffd01ece8f2d1a
ming <999aldwo@gmail.com>
2025-01-15 22:17:12 +0900

    fcm 연동하여 push알림 보내기

--- part3_carrot_market/lib/common/fcm/fcm_manager.dart
+++ part3_carrot_market/lib/common/fcm/fcm_manager.dart
@@ -4,4 +4,10 @@ class FcmManager {
   static void requestPermission() {
     FirebaseMessaging.instance.requestPermission();
   }
+
+  static void initialize() async {
+    // 테스트 메세지 전송 기능 사용 시 필요한 token 받아올 수 있음
+    final token = await FirebaseMessaging.instance.getToken();
+    // print(token);
+  }
 }
